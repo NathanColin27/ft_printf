dc7e6ed9d75da566100460a45eeb7705d68216f7
Nathan Colin <nathancolin27@gmail.com>
2020-02-23 18:13:39 +0100

    Need to work on minus flags for printd

--- srcs/print_d.c
+++ srcs/print_d.c
@@ -6,7 +6,7 @@
 /*   By: ncolin <ncolin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 16:15:21 by ncolin            #+#    #+#             */
-/*   Updated: 2020/02/23 17:45:18 by ncolin           ###   ########.fr       */
+/*   Updated: 2020/02/23 17:51:57 by ncolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,31 @@ void print_d(va_list *arg_list, t_flags *flags)
 		flags->zero = 0;
 	if(flags->width + lenght < flags->dot )
 		flags->width = 0;
-	ft_put_width(flags->width, flags->zero, lenght);
-	if(flags->dot >= 0)
+	if(flags->minus == 0)
 	{
-		while(flags->dot > lenght)
+		ft_put_width(flags->width, flags->zero, lenght);
+		if(flags->dot >= 0)
 		{
-			ft_putchar('0');
-			flags->dot--;
+			while(flags->dot > lenght)
+			{
+				ft_putchar('0');
+				flags->dot--;
+			}
 		}
+		ft_putnbr(num);
 	}
-	ft_putnbr(num);
+
+	else if(flags->minus == 1)
+	{
+		if(flags->dot >= 0)
+		{
+			while(flags->dot > lenght)
+			{
+				ft_putchar('0');
+				flags->dot--;
+			}
+		}
+		ft_putnbr(num);
+		ft_put_width(flags->width, flags->zero, lenght);
+	}
 }
